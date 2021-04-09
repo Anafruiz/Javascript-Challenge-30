bcd080fff3c797db014e1f5258080bae9d480d02
Anadbh <anafruiz20@gmail.com>
2021-04-09 14:17:24 +0200

    challenge 5

---
+++ Challenge 5/js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+const panels = document.querySelectorAll(".panel");
+function toggleOpen() {
+  console.log("hola");
+  this.classList.toggle("open");
+}
+function toggleOpenActive(e) {
+  console.log(e.propertyName);
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
+panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
+panels.forEach((panel) =>
+  panel.addEventListener("transitionend", toggleOpenActive)
+);
