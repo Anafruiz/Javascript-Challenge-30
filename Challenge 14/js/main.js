43a2a11f65463d6cb052a397fb29c09a207cbfd9
Anadbh <anafruiz20@gmail.com>
2021-04-20 13:03:32 +0200

    Challenge 14

---
+++ Challenge 14/js/main.js
@@ -0,0 +1,79 @@
+"use strict";
+
+// start with strings, numbers and booleans
+// let age = 100;
+// let age2 = age;
+// console.log(age, age2);
+// age = 200;
+// console.log(age, age2);
+// let name = "ana";
+// let name2 = name;
+// console.log(name, name2);
+// name = "wes";
+// console.log(name, name2);
+
+// Let's say we have an array
+const players = ["Wes", "Sarah", "Ryan", "Poppy"];
+
+// and we want to make a copy of it.
+const team = players;
+console.log(players, team);
+team[3] = "Lux";
+// You might think we can just do something like this:
+
+// however what happens when we update that array?
+
+// now here is the problem!
+
+// oh no - we have edited the original array too!
+
+// Why? It's because that is an array reference, not an array copy. They both point to the same array!
+
+// So, how do we fix this? We take a copy instead!
+const team2 = players.slice();
+team2[3] = "mary";
+// one way
+
+// or create a new array and concat the old one in
+const team3 = [].concat(players);
+
+// or use the new ES6 Spread
+const team4 = [...players];
+team4[2] = "hee";
+
+const team5 = Array.from(players);
+team5[1] = "cooool";
+// now when we update it, the original one isn't changed
+
+// The same thing goes for objects, let's say we have a person object
+
+// with Objects
+const person = {
+  name: "Wes Bos",
+  age: 80,
+};
+
+// and think we make a copy:
+const captain = person;
+captain.number = 99;
+
+// how do we take a copy instead?
+const cap2 = Object.assign({}, person, { number: 90, age: 12 });
+// We will hopefully soon see the object ...spread
+// const cap3={...person}; DOES NOT EXIT YET!!!
+// Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
+
+const wes={
+  name:"wes",
+  age:52,
+  social:{
+    twitter"@wesbos",
+    facebook:"wesbos.developer"
+  }
+}
+console.clear();
+console.log(wes);
+
+const dev = Object.assign({}, wes); 
+
+const dev2 = JSON.parse(JSON.stringify(wes));
