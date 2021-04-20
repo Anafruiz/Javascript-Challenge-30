382b67d347961fb86ac37719637c0fbb3866bcba
Anadbh <anafruiz20@gmail.com>
2021-04-20 10:25:02 +0200

    Challenge 13

---
+++ Challenge 13/js/main.js
@@ -0,0 +1,37 @@
+"use strict";
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function () {
+    var context = this;
+    var args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const sliderImages = document.querySelectorAll(".slide-in");
+
+window.addEventListener("scroll", debounce(checkSlide));
+
+function checkSlide(e) {
+  sliderImages.forEach((image) => {
+    //half way through the image
+    const slideInAt = window.scrollY + window.innerHeight - image.height / 2;
+    console.log(slideInAt);
+    //bottom of the image
+    const imageBottom = image.offsetTop + image.height;
+    const isHalfShown = slideInAt > image.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      image.classList.add("active");
+    } else {
+      image.classList.remove("active");
+    }
+  });
+}
